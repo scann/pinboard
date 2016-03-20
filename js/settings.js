24b3af71b97698ec9c8207ed817d673799e66c4a
scann <apple.eater01@gmail.com>
2016-03-20 20:48:00 +0200

    Presets window on main page

---
+++ js/settings.js
@@ -0,0 +1,6 @@
+var settings = document.getElementById('settings');
+var showSettings = document.getElementById('settings-block');
+
+settings.addEventListener('click', function(){
+    showSettings.style.display = 'block';
+});
