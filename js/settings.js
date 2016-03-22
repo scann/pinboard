1331996863f780918538ae1efa715cc47500a94d
Anna Krasnoshchok <apple.eater01@gmail.com>
2016-03-22 14:57:19 +0200

    Options for board and pin colors

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,10 @@
 var settings = document.getElementById('settings');
 var showSettings = document.getElementById('settings-block');
+var hideSettings = document.getElementsByClassName('close')[0];
 
 settings.addEventListener('click', function(){
     showSettings.style.display = 'block';
+});
+hideSettings.addEventListener('click', function(){
+	showSettings.style.display = 'none';
 });
